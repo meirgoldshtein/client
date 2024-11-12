f445bdff2f6147f53f1a130f80f0b61a9acbb75b
meirgoldshtein <m0548491057@gmail.com>
2024-11-12 11:50:40 +0200

    Added an automatic validation function behind the scenes if there is a token from the last connection

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -7,7 +7,9 @@ import userState from "../../types/userState";
 const initialData: userState = {
     error: null,
     status: dataStatus.IDLE,
-    user: null
+    user: null,
+    isAuthenticated: false
+
 }
 
 const fetchLogin = createAsyncThunk('user/login',
@@ -53,33 +55,69 @@ const fetchRegister = createAsyncThunk('user/register',
 
     })
 
+
+// Async thunk for checking auth status
+const checkAuth = createAsyncThunk(
+    'user/verify',
+    async (_, { rejectWithValue }) => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            return rejectWithValue('No token found');
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/api/users/verify', {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json',        
+                    'Authorization': token
+                 },
+            });
+
+            if (!response.ok) {
+                localStorage.removeItem('token');
+                return rejectWithValue('Invalid token');
+            }
+
+            const data = await response.json();
+            console.log('token', data.token)
+            localStorage.setItem('token', data.token)
+            return data.data;
+        } catch (error) {
+            return rejectWithValue('Auth check failed');
+        }
+    }
+);
+
 const userSlice = createSlice({
     name: 'user',
     initialState: initialData,
     reducers: {
-       updateVoteStatus: (state, action) => {
-           if (state.user) {
-               state.user.votedFor = action.payload
-               state.user.hasVoted = true
-           }
-       }
+        updateVoteStatus: (state, action) => {
+            if (state.user) {
+                state.user.votedFor = action.payload
+                state.user.hasVoted = true
+            }
+        }
     },
-    extraReducers: (builder: ActionReducerMapBuilder<userState>) => { 
+    extraReducers: (builder: ActionReducerMapBuilder<userState>) => {
         builder.addCase(fetchLogin.pending, (state) => {
-             state.status = dataStatus.LOADING 
-             state.error = null
-             state.user = null
+            state.status = dataStatus.LOADING
+            state.error = null
+            state.user = null
         }).addCase(fetchLogin.fulfilled, (state, action) => {
             console.log('login', action.payload)
             state.user = action.payload as unknown as IUser
             state.error = null
+            state.isAuthenticated = true
             state.status = dataStatus.SUCCESS
         }).addCase(fetchLogin.rejected, (state, action) => {
             state.error = action.error as string
             console.log(state.error)
+            state.isAuthenticated = false
             state.user = null
             state.status = dataStatus.FAILED
-        }).addCase(fetchRegister.fulfilled, (state, action) => {
+        }).addCase(fetchRegister.fulfilled, (state) => {
             state.user = null
             state.error = null
             state.status = dataStatus.SUCCESS
@@ -87,10 +125,26 @@ const userSlice = createSlice({
             state.error = action.error as string
             state.user = null
             state.status = dataStatus.FAILED
+        }).addCase(checkAuth.fulfilled, (state, action) => {
+            state.user = action.payload as unknown as IUser
+            state.status = dataStatus.SUCCESS
+            state.isAuthenticated = true
+            console.log("fulfilled")
+        }).addCase(checkAuth.rejected, (state, action) => {
+            state.error = action.error as string
+            state.user = null
+            state.isAuthenticated = false
+            state.status = dataStatus.FAILED
+            console.log("rejected")
+        }).addCase(checkAuth.pending, (state) => {
+            state.status = dataStatus.LOADING
+            state.error = null
+            state.user = null
+            console.log("pending")
         })
     }
 })
 
 export const { updateVoteStatus } = userSlice.actions
-export { fetchLogin, fetchRegister }
+export { fetchLogin, fetchRegister, checkAuth }
 export default userSlice
--- src/types/userState.ts
+++ src/types/userState.ts
@@ -5,6 +5,8 @@ interface userState {
     error: string | null;
     status: dataStatus;
     user: IUser | null;
+    isAuthenticated: boolean;
+
 }
 
 export default userState
